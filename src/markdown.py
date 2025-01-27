327833cd2979db4532608d26fe6ed75b04f51218
exequielyuri <leiuqxre@gmail.com>
2025-01-27 14:18:39 +0800

    feat(markdown): conversion to html node

--- src/markdown.py
+++ src/markdown.py
@@ -1,5 +1,8 @@
-from enum import Enum
 import re
+from enum import Enum
+from md_helpers import extract_md_blocks
+from parentnode import ParentNode
+from textnode import md_to_text_nodes
 
 class BlockType(Enum):
     PARAGRAPH = 'paragraph'
@@ -9,15 +12,15 @@ class BlockType(Enum):
     U_LIST = 'unordered_list'
     O_LIST = 'ordered_list'
 
-def md_to_block_type(md):
+def md_block_type(block):
     block_type = BlockType.PARAGRAPH
-    md_lines = list(map(lambda line: line.strip() , md.splitlines()))
+    md_lines = list(map(lambda line: line.strip() , block.splitlines()))
 
-    if md.startswith('#'):
+    if block.startswith('#'):
         block_type = BlockType.HEADING
-    elif md.startswith('```'):
+    elif block.startswith('```') and block.endswith('```'):
         block_type = BlockType.CODE
-    elif md.startswith('>'):
+    elif block.startswith('>'):
         block_type = BlockType.QUOTE
     elif all(line.startswith(('-', '*')) for line in md_lines):
         block_type = BlockType.U_LIST
@@ -25,3 +28,63 @@ def md_to_block_type(md):
         block_type = BlockType.O_LIST
 
     return block_type
+
+def md_to_html_node(md):
+    if md == '':
+        raise ValueError('MD file does not contain anything')
+
+    blocks = extract_md_blocks(md)
+    html_nodes = list(map(block_to_html_node, blocks))
+    return ParentNode('html', html_nodes)
+
+def block_to_html_node(block):
+    match md_block_type(block):
+        case BlockType.PARAGRAPH:
+            return ParentNode('p', children_nodes(block))
+        case BlockType.HEADING:
+            return block_to_heading(block)
+        case BlockType.CODE:
+            block = re.sub(r'```', '', block)
+            return ParentNode('pre', [ ParentNode('code', children_nodes(block)) ])
+        case BlockType.QUOTE:
+            block = re.sub(r'>\s*', '', block)
+            return ParentNode('blockquote', children_nodes(block))
+        case BlockType.U_LIST:
+            return block_to_u_list(block)
+        case BlockType.O_LIST:
+            return block_to_o_list(block)
+
+def block_to_heading(block):
+    match = re.match(r'(^#{1,6})\s*(.*)', block)
+
+    if match == None:
+        raise ValueError('Block is not of type heading.')
+
+    hashes = match.group(1)
+    block = match.group(2)
+    return ParentNode(f'h{len(hashes)}', children_nodes(block))
+
+def block_to_u_list(block):
+    def remove_bullets(item):
+        match = re.search(r'[-*]\s*(.*)', item.strip())
+        return match and match.group(1)
+
+    items = block.splitlines()
+    items = map(remove_bullets, items)
+    items = map(lambda item: ParentNode('li', children_nodes(item)), items)
+    return ParentNode('ul', list(items))
+
+def block_to_o_list(block):
+    def remove_numbering(item):
+        match = re.search(r'\d+\.\s*(.*)', item.strip())
+        return match and match.group(1)
+
+    items = block.splitlines()
+    items = map(remove_numbering, items)
+    items = map(lambda item: ParentNode('li', children_nodes(item)), items)
+    return ParentNode('ol', list(items))
+
+def children_nodes(block):
+    text_nodes = md_to_text_nodes(block)
+    children = list(map(lambda tn: tn.to_html_node(), text_nodes))
+    return children
--- src/test_markdown.py
+++ src/test_markdown.py
@@ -1,28 +1,159 @@
 import unittest
 from markdown import *
+from parentnode import ParentNode
+from leafnode import LeafNode
 
 class TestMDToBlockType(unittest.TestCase):
     def test_paragraph(self):
         md = 'text'
-        block_type = md_to_block_type(md)
+        block_type = md_block_type(md)
         self.assertEqual(block_type, BlockType.PARAGRAPH)
 
     def test_heading(self):
         md = '# text'
-        block_type = md_to_block_type(md)
+        block_type = md_block_type(md)
         self.assertEqual(block_type, BlockType.HEADING)
 
     def test_code(self):
         md = '```code```'
-        block_type = md_to_block_type(md)
+        block_type = md_block_type(md)
         self.assertEqual(block_type, BlockType.CODE)
 
     def test_u_list(self):
         md = '- a \n * b'
-        block_type = md_to_block_type(md)
+        block_type = md_block_type(md)
         self.assertEqual(block_type, BlockType.U_LIST)
 
     def test_o_list(self):
         md = '1. a \n 10. b'
-        block_type = md_to_block_type(md)
+        block_type = md_block_type(md)
         self.assertEqual(block_type, BlockType.O_LIST)
+
+class TestMDToHTMLNode(unittest.TestCase):
+    def test_empty_string(self):
+        with self.assertRaises(ValueError):
+            md_to_html_node('')
+
+    def test_paragraph(self):
+        md = 'text'
+        html_node = md_to_html_node(md)
+        expected_node = ParentNode('html', [ ParentNode('p', [ LeafNode(None, 'text') ]) ])
+        self.assertEqual(html_node, expected_node)
+
+    def test_heading_1(self):
+        md = '# text'
+        html_node = md_to_html_node(md)
+        expected_node = ParentNode('html', [ ParentNode('h1', [ LeafNode(None, 'text') ]) ])
+        self.assertEqual(html_node, expected_node)
+
+    def test_heading_1_no_space(self):
+        md = '#text'
+        html_node = md_to_html_node(md)
+        expected_node = ParentNode('html', [ ParentNode('h1', [ LeafNode(None, 'text') ]) ])
+        self.assertEqual(html_node, expected_node)
+
+    def test_heading_2(self):
+        md = '## text'
+        html_node = md_to_html_node(md)
+        expected_node = ParentNode('html', [ ParentNode('h2', [ LeafNode(None, 'text') ]) ])
+        self.assertEqual(html_node, expected_node)
+
+    def test_heading_3(self):
+        md = '### text'
+        html_node = md_to_html_node(md)
+        expected_node = ParentNode('html', [ ParentNode('h3', [ LeafNode(None, 'text') ]) ])
+        self.assertEqual(html_node, expected_node)
+
+    def test_heading_4(self):
+        md = '#### text'
+        html_node = md_to_html_node(md)
+        expected_node = ParentNode('html', [ ParentNode('h4', [ LeafNode(None, 'text') ]) ])
+        self.assertEqual(html_node, expected_node)
+
+    def test_heading_5(self):
+        md = '##### text'
+        html_node = md_to_html_node(md)
+        expected_node = ParentNode('html', [ ParentNode('h5', [ LeafNode(None, 'text') ]) ])
+        self.assertEqual(html_node, expected_node)
+
+    def test_heading_6(self):
+        md = '###### text'
+        html_node = md_to_html_node(md)
+        expected_node = ParentNode('html', [ ParentNode('h6', [ LeafNode(None, 'text') ]) ])
+        self.assertEqual(html_node, expected_node)
+
+    def test_heading_6_with_extra(self):
+        md = '####### text'
+        html_node = md_to_html_node(md)
+        expected_node = ParentNode('html', [ ParentNode('h6', [ LeafNode(None, '# text') ]) ])
+        self.assertEqual(html_node, expected_node)
+
+    def test_code(self):
+        md = '```text```'
+        html_node = md_to_html_node(md)
+        expected_node = ParentNode('html',
+                            [ ParentNode('pre',
+                                [ ParentNode('code', [ LeafNode(None, 'text') ])
+                            ])
+                        ])
+        self.assertEqual(html_node, expected_node)
+
+    def test_quote(self):
+        md = '> text'
+        html_node = md_to_html_node(md)
+        expected_node = ParentNode('html', [ ParentNode('blockquote', [ LeafNode(None, 'text') ]) ])
+        self.assertEqual(html_node, expected_node)
+
+    ##### U_LIST
+    # '-a \n *b \n -c'
+    #   =>  ParentNode(
+    #           tag: 'html',
+    #           children: [
+    #               ParentNode(
+    #                   'ul',
+    #                   [
+    #                       LeafNode('li', 'a'),
+    #                       LeafNode('li', 'b'),
+    #                       LeafNode('li', 'c')
+    #                   ]
+    #           ]
+    #       )
+    def test_u_list(self):
+        md = '-a \n *b \n -c'
+        html_node = md_to_html_node(md)
+        expected_node = ParentNode('html', [
+                            ParentNode('ul',
+                                [
+                                    ParentNode('li', [ LeafNode(None, 'a') ]),
+                                    ParentNode('li', [ LeafNode(None, 'b') ]),
+                                    ParentNode('li', [ LeafNode(None, 'c') ])
+                                ])
+                        ])
+        self.assertEqual(html_node, expected_node)
+
+    ##### O_LIST
+    # '1.a \n 2.b \n 3.c'
+    #   =>  ParentNode(
+    #           tag: 'html',
+    #           children: [
+    #               ParentNode(
+    #                   'ol',
+    #                   [
+    #                       LeafNode('li', 'a'),
+    #                       LeafNode('li', 'b'),
+    #                       LeafNode('li', 'c')
+    #                   ]
+    #           ]
+    #       )
+    def test_o_list(self):
+        md = '1. a \n 2.b \n 3.c \n'
+        html_node = md_to_html_node(md)
+        expected_node = ParentNode('html', [
+                            ParentNode('ol',
+                                [
+                                    ParentNode('li', [ LeafNode(None, 'a') ]),
+                                    ParentNode('li', [ LeafNode(None, 'b') ]),
+                                    ParentNode('li', [ LeafNode(None, 'c') ])
+                                ])
+                        ])
+        self.assertEqual(html_node, expected_node)
