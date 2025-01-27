2f9a458eb7929945e63fcb89b2021873cc23adbe
exequielyuri <leiuqxre@gmail.com>
2025-01-27 14:36:32 +0800

    feat(main): generation of public dir

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
+import os
+import shutil
 from textnode import TextNode, TextType
 
+def copy_dir(src_path, dst_path):
+    if os.path.exists(dst_path):
+        shutil.rmtree(dst_path)
+    os.mkdir(dst_path)
+
+    for name in os.listdir(src_path):
+        curr_src_path = os.path.join(src_path, name)
+        curr_dst_path = os.path.join(dst_path, name)
+
+        if os.path.isfile(curr_src_path):
+            shutil.copy(curr_src_path, dst_path)
+        else:
+            copy_dir(curr_src_path, curr_dst_path)
+
 def main():
-    text_node = TextNode('This is a text node', TextType.BOLD, 'https://boot.dev')
-    print(text_node)
+    copy_dir('static', 'public')
+
 
 main()
