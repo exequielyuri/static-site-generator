4d1633ca58a55d87dcd234d89a40e86a70ad522a
exequielyuri <leiuqxre@gmail.com>
2025-01-30 17:36:19 +0800

    feat(main): recursive generation of pages

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import pathlib
 from markdown import extract_title, md_to_html_node
 
 def copy_dir(src_path, dst_path):
@@ -16,11 +17,11 @@ def copy_dir(src_path, dst_path):
         else:
             copy_dir(curr_src_path, curr_dst_path)
 
-def generate_page(src_path, template_path, dst_path):
+def generate_page(src_path, tpl_path, dst_path):
     with open(src_path, 'r') as f:
         md = f.read()
 
-    with open(template_path, 'r') as f:
+    with open(tpl_path, 'r') as f:
         template = f.read()
 
     title = extract_title(md)
@@ -31,8 +32,22 @@ def generate_page(src_path, template_path, dst_path):
     with open(dst_path, 'w') as f:
         f.write(template)
 
+def generate_pages_recursively(src_dir, tpl_path, dst_dir):
+    items = os.listdir(src_dir)
+    for item in items:
+        src_path = os.path.join(src_dir, item)
+        path = pathlib.Path(src_path)
+        if path.is_file():
+            if path.suffix != '.md':
+                continue
+            curr_dst_path = os.path.join(dst_dir, f'{path.stem}.html')
+            generate_page(src_path, tpl_path, curr_dst_path)
+        else:
+            curr_dst_dir = os.path.join(dst_dir, item)
+            generate_pages_recursively(src_path, tpl_path, curr_dst_dir)
+
 def main():
     copy_dir('static', 'public')
-    generate_page('content/index.md', 'template.html', 'public/index.html')
+    generate_pages_recursively('content', 'template.html', 'public')
 
 main()
